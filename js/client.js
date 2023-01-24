b4a943da1dfa938de377c83ac789dad20353456e
ashutosh-karn <karnashu23@gmail.com>
2023-01-25 01:59:19 +0530

    commit-3

--- js/client.js
+++ js/client.js
@@ -1,12 +1,15 @@
-// const socket = io('http://localhost:8000');
+// const socket = io('http://localhost:8000'); {doesn't work due to CORS policy}
 const socket = io('http://localhost:8000',{transports:['websocket']});
 
+//Get DOM in the respective JavaScript variables
 const form = document.getElementById('send-container');
 const messageInp =  document.getElementById('messageInp')
 const messageContainer = document.querySelector(".container")
 
+//Notification tone which will play when a message is received
 var audio = new Audio('Notification.mp3');
 
+//Function which will append events information to the container
 const append = (message, position)=>{
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
@@ -18,6 +21,26 @@ const append = (message, position)=>{
     }
 }
 
+//Ask new user their name before joining, and let the server know
+const names = prompt("Enter your name to join the chat...");
+socket.emit('new-user-joined', names);
+
+//If user joins, the server receives their name
+socket.on('user-joined', names =>{
+    append(`${names} joined the chat`, 'centerJoined');
+})
+
+//If server sends a message, it is received here
+socket.on('receive', data =>{
+    append(`${data.names}: ${data.message}`, 'left');
+})
+
+//If a user leaves the chat, append the information to the container
+socket.on('leftChat', names =>{
+    append(`${names} left the chat`, 'centerLeft');
+})
+
+//If the form is submitted, send the message to server
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     const message = messageInp.value;
@@ -25,20 +48,3 @@ form.addEventListener('submit', (e)=>{
     socket.emit('send', message);
     messageInp.value = '';
 })
-
-const names = prompt("Enter your name to join the chat...");
-socket.emit('new-user-joined', names);
-
-
-socket.on('user-joined', names =>{
-    append(`${names} joined the chat`, 'centerJoined');
-})
-
-socket.on('receive', data =>{
-    append(`${data.names}: ${data.message}`, 'left');
-})
-
-socket.on('leftChat', names =>{
-    append(`${names} left the chat`, 'centerLeft');
-})
-
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -3,15 +3,16 @@ const io = require('socket.io')(8000);
 const users = {};
 
 io.on('connection', socket =>{
+    //If new user joins, broadcast joining message with users name to all other online members
     socket.on('new-user-joined', names=>{
         users[socket.id] = names;
         socket.broadcast.emit('user-joined', names);
     });
-
+    //When a user sends a message, broadcast it to every joined user
     socket.on('send', message =>{
         socket.broadcast.emit('receive', {message: message, names: users[socket.id]})
     });
-
+    //If a user leaves the chat, broadcast it with that user's name
     socket.on('disconnect', message =>{
         socket.broadcast.emit('leftChat', users[socket.id])
         delete users[socket.id];
